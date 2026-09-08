ef4dcff97fe910d695e1d773b66fcbe8b7b41d55
richie-rich90454 <rjiang880@gmail.com>
2026-09-08 11:04:49 +0800

    feat: add welcome message

---
+++ MovieTicketKiosk.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class MovieTicketKiosk{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Starlight Cinema Kiosk!");
+        
+    }
+}
