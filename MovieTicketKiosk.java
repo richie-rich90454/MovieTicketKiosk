fefa9d8650fc3baf7ba3bba6c41280e05a790406
richie-rich90454 <rjiang880@gmail.com>
2026-09-08 11:08:02 +0800

    feat: scaffold MovieTicketKioskUserRequirements class

--- MovieTicketKiosk.java
+++ MovieTicketKiosk.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class MovieTicketKiosk{
     public static void main(String[] args){
-        System.out.println("Welcome to the Starlight Cinema Kiosk!");
+        System.out.println("Welcome to the Starlight Cinema Kiosk!\n");
+        MovieTicketKioskUserRequirements userRequirements=new MovieTicketKioskUserRequirements();
         
     }
 }
---
+++ MovieTicketKioskUserRequirements.java
@@ -0,0 +1,22 @@
+public class MovieTicketKioskUserRequirements {
+    private int ticketCount;
+    private int popcornCount;
+    private int drinkCount;
+    private String username;
+    private String movieTitle;
+    public void setTicketCount(){
+
+    }
+    public void setPopcornCount(){
+        
+    }
+    public void setDrinkCount(){
+        
+    }
+    public void setUsername(){
+        
+    }
+    public void setMovieTitle(){
+        
+    }
+}
