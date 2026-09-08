3c78f1a5e111cbcf1e6dbede47e88f74b7e241a4
richie-rich90454 <rjiang880@gmail.com>
2026-09-08 11:20:31 +0800

    chore: complete generateBookingCodeAndSeating method

--- MovieTicketKioskUserRequirements.java
+++ MovieTicketKioskUserRequirements.java
@@ -5,7 +5,7 @@ public class MovieTicketKioskUserRequirements {
     private String username;
     private String movieTitle;
     private String bookingCode;
-    private int[] seat;
+    private int[] seat;//index 0 denoting row while index 1 denotes seat number
     public void setTicketCount(int ticketCount){
         this.ticketCount=ticketCount;
     }
@@ -22,8 +22,14 @@ public class MovieTicketKioskUserRequirements {
         this.movieTitle=movieTitle;
     }
     public void generateBookingCodeAndSeating(){
+        int randomUserCode=(int)(Math.random()*900)+100;
+        if (movieTitle.length()<=3){
+            bookingCode=movieTitle+randomUserCode;
+        }
+        else{
+            bookingCode=movieTitle.substring(0, 4)+randomUserCode;
+        }
         seat[0]=(int)(Math.random()*10)+1;
         seat[1]=(int)(Math.random()*20)+1;
-
     }
 }
